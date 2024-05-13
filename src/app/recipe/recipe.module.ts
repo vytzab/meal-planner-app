3bd62560e76861832cb0acd67b93104554361235
vytzab <vytautas.zabielskas@gmail.com>
2024-05-13 11:57:16 +0300

    Created recipe module and component

---
+++ src/app/recipe/recipe.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipeComponent } from './recipe.component';
+
+
+
+@NgModule({
+  declarations: [
+    RecipeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RecipeModule { }
