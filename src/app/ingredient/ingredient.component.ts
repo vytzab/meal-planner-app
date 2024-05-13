b4acc20984e72c8df940174b5468b19cdea74ca1
vytzab <vytautas.zabielskas@gmail.com>
2024-05-13 11:56:26 +0300

    Created ingredient module and component

---
+++ src/app/ingredient/ingredient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient',
+  templateUrl: './ingredient.component.html',
+  styleUrl: './ingredient.component.css'
+})
+export class IngredientComponent {
+
+}
---
+++ src/app/ingredient/ingredient.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IngredientComponent } from './ingredient.component';
+
+
+
+@NgModule({
+  declarations: [
+    IngredientComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IngredientModule { }
