1013cd19e3309d5555e50460f61862a74bd9037f
vytzab <vytautas.zabielskas@gmail.com>
2024-05-14 14:24:53 +0300

    Implemented recipe service

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from '../models/recipe';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  private apiUrl = "http://localhost:9097/recipe"
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(this.apiUrl + "/getAllRecipes");
+  }
+
+  getRecipe(id: number): Observable<Recipe> | undefined {
+    return this.http.get<Recipe>(this.apiUrl + "/getRecipe/" + id);
+  }
+
+  addRecipe(recipe: Recipe): Observable<void> {
+    return this.http.post<void>(this.apiUrl + "/addRecipe", recipe)
+  }
+
+  updateRecipe(updatedRecipe: Recipe): Observable<void> {
+    return this.http.put<void>(this.apiUrl + "/updateRecipe", updatedRecipe)
+  }
+
+  deleteRecipe(id: number): Observable<void> {
+    return this.http.delete<void>(this.apiUrl + "/deleteRecipe/" + id)
+  }
 }
