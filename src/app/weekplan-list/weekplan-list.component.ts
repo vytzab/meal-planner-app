78b1c09e35c41acc2b903599ab9980b50bb244f4
vytzab <vytautas.zabielskas@gmail.com>
2024-07-23 10:05:36 +0300

    Weekplan List initial implementation

--- src/app/weekplan-list/weekplan-list.component.ts
+++ src/app/weekplan-list/weekplan-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeekplanService } from '../weekplan/weekplan.service';
+import { Weekplan } from '../models/weekplan';
 
 @Component({
   selector: 'app-weekplan-list',
   templateUrl: './weekplan-list.component.html',
-  styleUrl: './weekplan-list.component.css'
+  styleUrls: ['./weekplan-list.component.css']
 })
-export class WeekplanListComponent {
+export class WeekplanListComponent implements OnInit {
+  weekplans: Weekplan[] = [];
 
+  constructor(private weekplanService: WeekplanService) { }
+
+  ngOnInit(): void {
+    this.fetchWeekplans();
+  }
+
+  fetchWeekplans(): void {
+    this.weekplanService.getWeekplans().subscribe({
+      next: (weekplans) => {
+        this.weekplans = weekplans;
+        console.log('Weekplans fetched:', this.weekplans);
+      },
+      error: (err) => console.error('Failed to fetch weekplans:', err),
+      complete: () => console.log('Weekplan fetch complete.')
+    });
+  }
 }
